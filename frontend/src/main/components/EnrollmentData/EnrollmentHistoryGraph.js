fc2b0b2fee9a7f6703825997289c633a4e4a01c4
Isaac Landry <ilandry@ucsb.edu>
2025-05-21 19:22:01 -0700

    Fixed formatting.

--- frontend/src/main/components/EnrollmentData/EnrollmentHistoryGraph.js
+++ frontend/src/main/components/EnrollmentData/EnrollmentHistoryGraph.js
@@ -12,7 +12,9 @@ import {
 // Stryker disable all: This is not to be run until the tests are written as it is a placeholder.
 
 export const formatTooltip = (value, _, props) => {
-  return [`Enrollment: ${value.toFixed(1)}, Date Created: ${props.payload.count}`];
+  return [
+    `Enrollment: ${value.toFixed(1)}, Date Created: ${props.payload.count}`,
+  ];
 };
 
 export const createCompleteEnrollmentData = (data) => {
@@ -62,7 +64,10 @@ const EnrollmentHistoryGraphs = ({ _enrollmentHistory }) => {
 
   return (
     <div data-testid="enrollment-history-graphs">
-      <EnrollmentHistoryLineChart data={enrollmentHistory} title={"Title to be implemented"} />
+      <EnrollmentHistoryLineChart
+        data={enrollmentHistory}
+        title={"Title to be implemented"}
+      />
     </div>
   );
 };
